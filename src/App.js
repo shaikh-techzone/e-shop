e89beff6beb55fc0ea79e4e5e3988e634b1bd6e2
Hafiz Shaikh Abdul Sami <95353717+shaikh-techzone@users.noreply.github.com>
2022-09-13 20:55:39 +0500

    React Router Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-// import logo from './logo.svg';
 import './App.css';
-import Checkout from './components/Checkout/Checkout';
-import Layout from './components/Layout/Layout';
-import Contact from './components/Contact/Contact';
-import Wishlist from './components/Wishlist/Wishlist';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import CheckoutPage from './Pages/CheckoutPage';
+import ContactPage from './Pages/ContactPage';
+import WishlistPage from './Pages/WishlistPage';
+import CartPage from './Pages/CartPage';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <>
-      <Layout>
-      <h1>Hallow world</h1>
-      </Layout>
-    
-   
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/checkout' element={<CheckoutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='/wishlist' element={<WishlistPage />} />
+            <Route path='/cart' element={<CartPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+
     </>
   );
 }
